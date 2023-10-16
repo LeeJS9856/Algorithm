7cfdea60f4d04a8786d8f85f536b8b55caf996ea
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2023-10-16 23:10:48 +0900

    [Bronze V] Title: A+B - 3, Time: 228 ms, Memory: 17912 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] arg) {
+		Scanner scanner = new Scanner(System.in);
+		int T;
+		T = scanner.nextInt();
+		int[] A = new int[T];
+		int[] B = new int[T];
+		
+		for(int i = 0;i<T;i++)
+		{
+			A[i] = scanner.nextInt();
+			B[i] = scanner.nextInt();
+		}
+		for(int i = 0;i<T;i++) {
+			System.out.println(A[i]+B[i]);
+		}
+		
+	}
+}
