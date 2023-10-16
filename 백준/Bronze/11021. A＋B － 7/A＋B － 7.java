8f99fe1eb30746a5411bbc0d1b9870bbf8a6f502
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2023-10-16 23:11:21 +0900

    [Bronze V] Title: A+B - 7, Time: 160 ms, Memory: 16072 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] arg) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(bf.readLine());
+		int[] A = new int[T];
+		int[] B = new int[T];
+		
+		for(int i=0;i<T;i++) {
+			String ab = bf.readLine();
+			StringTokenizer st = new StringTokenizer(ab);
+			A[i] = Integer.parseInt(st.nextToken());
+			B[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		for(int i=0;i<T;i++) {
+			int sum = A[i] + B[i];
+			bw.write("Case #" + (i+1) + ": "+sum+"\n");
+		}
+		bw.flush();
+		bw.close();
+		
+	}
+}
