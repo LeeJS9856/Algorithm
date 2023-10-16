13ff1738819ce822dc8b035fe3eaa71be3a2b9ed
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2023-10-16 23:10:36 +0900

    [Bronze V] Title: ??!, Time: 216 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] arg) {
+		Scanner scanner = new Scanner(System.in);
+		String name;
+		name = scanner.next();
+		
+		System.out.println(name+"??!");
+		
+	}
+}
