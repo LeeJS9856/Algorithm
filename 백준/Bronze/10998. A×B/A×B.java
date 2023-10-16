614366b2b2cdea0ac00ba65ffbb174948786e414
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2023-10-16 23:11:13 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] arg) {
+		Scanner scanner = new Scanner(System.in);
+		int a, b;
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		
+		System.out.println(a*b);
+	}
+}
