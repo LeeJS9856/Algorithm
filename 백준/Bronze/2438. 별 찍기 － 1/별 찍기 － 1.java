fca984b418ca2a7dc700804d380e35d0ccc950d8
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2023-10-16 23:06:22 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] arg) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(bf.readLine());
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for(int i=1;i<=N;i++) {
+			for(int j=1;j<=i;j++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
