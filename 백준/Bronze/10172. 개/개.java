d22a63b70d1bcbdac4cf640007ef9af589062705
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2023-10-16 23:08:59 +0900

    [Bronze V] Title: 개, Time: 112 ms, Memory: 13844 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] arg) {
+		System.out.println(
+				"|\\_/|\n"+
+				"|q p|   /}\n"
+				+"( 0 )\"\"\"\\\n"
+				+"|\"^\"`    |\n"
+				+"||_/=\\\\__|\n");
+	}
+}
