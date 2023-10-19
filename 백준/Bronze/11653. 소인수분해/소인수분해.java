7e1016789acdfbe96145519b277ef3a87ce07647
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2023-10-19 13:18:10 +0900

    [Bronze I] Title: 소인수분해, Time: 200 ms, Memory: 15908 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		while(N!=1) {
+			for(int i=2;i<=N;i++) {
+				if(N%i==0) {
+					bw.write(i+"\n");
+					N = N / i;
+					break;
+				}
+			}
+			
+		}
+		bw.flush();
+		bw.close();
+	}
+	
+}
