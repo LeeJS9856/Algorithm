31eba603d94402459877d269bdecbe03254ff58e
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2023-10-19 22:45:31 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 144 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		long n = Integer.parseInt(br.readLine());
+		long res = (n-2)*(n-1)*n/6;
+		System.out.print(res+"\n"+3);
+	}
+}
