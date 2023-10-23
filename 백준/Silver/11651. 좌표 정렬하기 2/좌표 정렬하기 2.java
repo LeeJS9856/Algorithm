d1ab6998803c016e84896148a5a40fc82b224bb9
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2023-10-23 20:36:36 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2392 ms, Memory: 323088 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		String[] xy = new String[N];
+		for(int i=0;i<N;i++) {
+			xy[i] = br.readLine();
+		}
+		
+		Arrays.sort(xy, new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				StringTokenizer st1 = new StringTokenizer(s1);
+				StringTokenizer st2 = new StringTokenizer(s2);
+				int x1 = Integer.parseInt(st1.nextToken());
+				int y1 = Integer.parseInt(st1.nextToken());
+				int x2 = Integer.parseInt(st2.nextToken());
+				int y2 = Integer.parseInt(st2.nextToken());
+				
+				if(y1==y2) return x1-x2;
+				else return y1-y2;
+			}
+		});
+		
+		for(String i : xy) {
+			System.out.println(i);
+		}
+	}
+}
