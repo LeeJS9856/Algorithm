98c9b4a65331fc1ebecc694fbfc31c4d681a7750
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2023-10-23 18:24:22 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1792 ms, Memory: 385116 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		int[] numberList = new int[10000];
+		Arrays.fill(numberList, 0);
+		for(int i=0;i<N;i++) {
+			numberList[Integer.parseInt(br.readLine())-1]++;
+		}
+		
+		for(int i=0;i<10000;i++) {
+			if(numberList[i]!=0) {
+				for(int j=0;j<numberList[i];j++)sb.append((i+1)+"\n");
+			}
+		}
+		bw.write(sb+"\n");
+		bw.flush();
+		bw.close();
+	}
+}
