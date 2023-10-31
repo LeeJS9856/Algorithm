635403fd187a895761901984efa321dba8b99dc4
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2023-10-31 23:45:36 +0900

    [Bronze IV] Title: 베라의 패션, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		bw.write(N*(N-1)+"");
+		bw.flush();
+		bw.close();
+	}
+
+}
