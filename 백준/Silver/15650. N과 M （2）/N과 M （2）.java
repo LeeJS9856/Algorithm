1fad0cd84a32f59ec96881ce43688b9c60c46186
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2023-11-06 01:27:25 +0900

    [Silver III] Title: N과 M (2), Time: 156 ms, Memory: 15844 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		List<Integer> list = new ArrayList<>();
+		com(N,M,1,list);
+		
+	} 
+	
+	public static void com(int n, int m,int k, List<Integer> list) {
+		if(m==0) {
+			for(int i:list) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+			
+			return;
+		}
+		for(int i=k;i<=n;i++) {
+			list.add(i);
+			com(n,m-1,i+1, list);
+			list.remove(list.size()-1);
+		}
+	}
+	
+	
+}
