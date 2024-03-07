e98ecb40f3a6b3cfc118209b0051b688cb1d6d84
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2024-03-07 10:13:21 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.py
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.py
@@ -1,8 +1,2 @@
 def solution(myString):
-    answer = ''
-    for i in range(len(myString)):
-        if ord('A') <= ord(myString[i]) <= ord('Z'):
-            answer += chr(ord(myString[i])+ord('a')-ord('A'))
-        else:
-            answer += myString[i]
-    return answer
+    return myString.lower()
