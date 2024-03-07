a3d6f90acc41bb9149568532ec5b74d77aa035e7
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2024-03-07 09:39:56 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,9 +1,3 @@
-import math
-
-
 def solution(n):
-    for i in range(math.floor(n**(1/2)), math.floor(n**(1/2))+1): #루트 n이 정수인지 판별
-        if i**2 == n:
-            return 1
-        else :
-            return 2
+    for i in range(int(n**(1/2)), int(n**(1/2))+1): #루트 n이 정수인지 판별
+        return 1 if i**2 == n else 2
